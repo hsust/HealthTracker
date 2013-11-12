17b569acd6d263055d453fb15dc9860261c6a9e9
Stephanie Hsu <stephanie.hsu888@gmail.com>
2013-11-11 22:14:36 -0500

    Copied code from MiscDemo for data manager to HealthTracker.

--- HealthTracker/SYHDataManager.m
+++ HealthTracker/SYHDataManager.m
@@ -16,4 +16,42 @@
 //    
 //}
 
+-(NSManagedObjectContext *)managedObjectContext
+{
+    if (!_managedObjectContext) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        _managedObjectContext.persistentStoreCoordinator = self.persistentStorecoordinator;
+    }
+    return _managedObjectContext;
+}
+
+//create data model in GUI. this is what data is supposed to look like
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (!_managedObjectModel) {
+        //grab data from app and load into model
+        NSURL *storeURL = [[NSBundle mainBundle] URLForResource:@"UserModel" withExtension:@"momd"];
+        _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:storeURL];
+    }
+    return _managedObjectModel;
+}
+
+//create sqlite database
+- (NSPersistentStoreCoordinator *)persistentStorecoordinator
+{
+    if (!_persistentStorecoordinator) {
+        NSURL *storeURL = [[self applicationDirectory] URLByAppendingPathComponent:@"Users.sqlite"];
+        
+        NSError *error;
+        _persistentStorecoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+        [_persistentStorecoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error];
+    }
+    return _persistentStorecoordinator;
+}
+
+- (NSURL *)applicationDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]lastObject];
+}
+
 @end
