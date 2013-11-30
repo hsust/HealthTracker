1c3c5aa71f28280ea9e38bdb6edaa718e490172c
Xinran Wang <xw1992@gmail.com>
2013-11-29 22:12:19 -0500

    Setup second view and got a sample graph to show.

---
+++ HealthTracker/SYHMealsGraphViewController.h
@@ -0,0 +1,14 @@
+//
+//  SYHMealsGraphViewController.h
+//  HealthTracker
+//
+//  Created by User on 11/29/13.
+//  Copyright (c) 2013 Stephanie Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CorePlot-CocoaTouch.h"
+
+@interface SYHMealsGraphViewController : UIViewController <CPTPlotDataSource>
+- (IBAction)dismissModal:(UIBarButtonItem *) sender;
+@end
